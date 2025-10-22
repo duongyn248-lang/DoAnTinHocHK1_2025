206b2465ac6c348013b0d39c02da1aaf352c7f55
Thùy Dương DH52300323 <duongyn248@gmail.com>
2025-10-23 06:39:40 +0700

    Update DocVaGhi.py

--- DocVaGhi.py
+++ DocVaGhi.py
@@ -1,22 +1,22 @@
 import csv
 
-def copy_csv(input_file, output_file):
-    
+def ghi_csv(input_file, output_file):
     with open(input_file, mode='r', newline='', encoding='utf-8') as infile:
-        reader = csv.reader(infile)  
+        reader = csv.DictReader(infile)      
+        fieldnames = reader.fieldnames        
 
         with open(output_file, mode='w', newline='', encoding='utf-8') as outfile:
-            writer = csv.writer(outfile)
-
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+            writer.writeheader()             
             for row in reader:
-                writer.writerow(row)
+                writer.writerow(row)          
 
-    print(f" Đã sao chép toàn bộ dữ liệu từ '{input_file}' sang '{output_file}'.")
+    print(f" Đã ghi toàn bộ dữ liệu từ '{input_file}' sang '{output_file}'.")
 
 
 if __name__ == "__main__":
     input_file = 'healthcare-dataset-stroke-data.csv'  
-    output_file = 'stroke_copy.csv'                      
+    output_file = 'ghi_csv.csv'                          
+    ghi_csv(input_file, output_file)
 
-    copy_csv(input_file, output_file)
 
